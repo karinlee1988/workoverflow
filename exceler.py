beb1e56b6d42c5dbc432047382c0e2bf9d40d7f1
karinlee <263384909@qq.com>
2020-10-07 16:58:47 +0800

    feat(exceler.py):split_workbook_by_column()函数用于按列拆分一个工作薄为多个工作薄，表头自定义行数

--- exceler.py
+++ exceler.py
@@ -8,15 +8,43 @@
 # @github : https://github.com/karinlee1988/
 # @gitee : https://gitee.com/karinlee/
 
+"""
+本模块用于对excel表格进行各种处理操作
+"""
 import pandas as pd
 
-def split_workbook_by_column_field(workbook_path,sheet_index,title_index,column_index):
+def split_workbook_by_column(workbook_path,sheet_index,title_index,column_index):
+    """
 
-    wb = pd.read_excel(io=workbook_path,sheet_name=sheet_index,header=title_index)  # 打开工作薄
-    col = wb.iloc[:,column_index].unique()  # 对单位列去重复
+    @param workbook_path: 要拆分的工作薄文件路径
+    @type workbook_path:  str
+
+    @param sheet_index: 要拆分的工作薄的工作表序号（从0开始）
+    @type sheet_index:  int
+
+    @param title_index: 拆分后表头的行数（从1开始）
+    @type title_index:  int
+
+    @param column_index: 根据某列的列索引号进行拆分（从0开始）
+    @type column_index: int
+    """
+    # 将要拆分的工作薄读取为dataframe对象
+    workbook_df = pd.read_excel(io=workbook_path,sheet_name=sheet_index,header=None)
+    # 获取工作薄的表头部分和数据部分，生成2个dataframe
+    workbook_header_df = workbook_df[0:title_index]
+    workbook_source_df = workbook_df[title_index:]
+    # 对单位列去重复,返回一个numpy数组col，包含所有不重复的列字段
+    col = workbook_source_df.iloc[:,column_index].unique()
+    # 遍历数组
     for x in col:
-        child_wb = wb[wb.iloc[:,column_index] == x]  # 循环，得到每一个单位列表，
-        child_wb.to_excel(x + '.xlsx', index=False)  # 将得到的表保存成Excel格式
+        # 先获取表头dataframe的拷贝
+        each_workbook_header_df = workbook_header_df.copy()
+        # 循环，得到每一个列字段的dataframe
+        each_workbook_source_df = workbook_source_df[workbook_source_df.iloc[:,column_index] == x]
+        # 拼接dataframe，表头部分与数据部分
+        each_workbook_df = each_workbook_header_df.append(each_workbook_source_df)
+        # 将得到的dataframe保存成Excel格式
+        each_workbook_df.to_excel(x + '.xlsx', index=False,header=False)
 
 if __name__ == '__main__':
-    split_workbook_by_column_field("示例.xlsx",0,1,3)
+    split_workbook_by_column("示例.xlsx",0,1,3)
