1e63caab65df7c7d69312930baef5405f13c128d
karinlee <263384909@qq.com>
2020-10-07 16:01:46 +0800

    feat(exceler.py):新增split_workbook_by_column_field()函数用于按列拆分一个工作薄为多个工作薄，表头为1行

--- exceler.py
+++ exceler.py
@@ -8,4 +8,15 @@
 # @github : https://github.com/karinlee1988/
 # @gitee : https://gitee.com/karinlee/
 
+import pandas as pd
 
+def split_workbook_by_column_field(workbook_path,sheet_index,title_index,column_index):
+
+    wb = pd.read_excel(io=workbook_path,sheet_name=sheet_index,header=title_index)  # 打开工作薄
+    col = wb.iloc[:,column_index].unique()  # 对单位列去重复
+    for x in col:
+        child_wb = wb[wb.iloc[:,column_index] == x]  # 循环，得到每一个单位列表，
+        child_wb.to_excel(x + '.xlsx', index=False)  # 将得到的表保存成Excel格式
+
+if __name__ == '__main__':
+    split_workbook_by_column_field("示例.xlsx",0,1,3)
