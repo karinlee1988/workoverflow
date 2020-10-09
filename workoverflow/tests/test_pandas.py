b64f20589ae4c1d28a9b304e735827d3d84933c7
karinlee <263384909@qq.com>
2020-10-09 19:34:33 +0800

    test(test_pandas.py)：新增测试文件

---
+++ workoverflow/tests/test_pandas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/10/9 19:31
+# @Author : 李加林
+# @FileName : test_pandas.py
+# @Software : PyCharm
+# @Blog : https://blog.csdn.net/weixin_43972976
+# @github : https://github.com/karinlee1988/
+# @gitee : https://gitee.com/karinlee/
+# @Personal website : https://karinlee.cn/
+
+import pandas as pd
+
