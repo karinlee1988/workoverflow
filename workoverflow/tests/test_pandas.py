7fd9d4ed000db9fd6155f17417a7646f71e31ed6
karinlee <263384909@qq.com>
2020-10-09 21:47:05 +0800

    test(test_pandas.py):用于pandas库测试

--- workoverflow/tests/test_pandas.py
+++ workoverflow/tests/test_pandas.py
@@ -10,4 +10,19 @@
 # @Personal website : https://karinlee.cn/
 
 import pandas as pd
+df1 = pd.read_excel("1.xlsx",header=1)
+df2 = pd.read_excel("2.xlsx",header=1)
+print(df1,"\n",df2)
+print("----------------------------")
+# 取df1有而df2没有的
+dfmerge_1 = pd.concat([df1, df2, df2], axis = 0, ignore_index=True)
+dfmerge_1 = dfmerge_1.drop_duplicates(subset="水果", keep=False)
+#取df2有但df1没有的
+print(dfmerge_1)
+print("----------------------------")
+dfmerge_2 = pd.concat([df2, df1, df1], axis = 0, ignore_index=True)
+dfmerge_2 = dfmerge_2.drop_duplicates(subset="水果", keep=False)
+print(dfmerge_2)
+df_all = dfmerge_1.append(dfmerge_2)
+print(df_all)
 
