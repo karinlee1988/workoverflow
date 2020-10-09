b2744015617ca2fa3d0bd72e47cec0072662a6e5
karinlee <263384909@qq.com>
2020-10-09 09:57:22 +0800

    style(__init__.py):更改文件头注释

--- workoverflow/__init__.py
+++ workoverflow/__init__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Time : 2020/10/3 16:08
-# @Author : karinlee
+# @Author : 李加林
 # @FileName : __init__.py
 # @Software : PyCharm
 # @Blog : https://blog.csdn.net/weixin_43972976
+# @github : https://github.com/karinlee1988/
+# @gitee : https://gitee.com/karinlee/
+# @Personal website : https://karinlee.cn/
 
 __author__ = '李加林'
 __all__ = ['dataprocess', 'exceler', 'fileio','workbookfunction']
