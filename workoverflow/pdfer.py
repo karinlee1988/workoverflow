e604256a3cafbdf0b91492bce46acd1d5febcbe0
karinlee <263384909@qq.com>
2020-12-12 15:21:32 +0800

    feat: 新增多图片合成pdf函数

--- workoverflow/pdfer.py
+++ workoverflow/pdfer.py
@@ -9,37 +9,83 @@
 # @gitee : https://gitee.com/karinlee/
 # @Personal website : https://karinlee.cn/
 
-
+import os
 from fpdf import FPDF
 from PIL import Image
 from workoverflow.fileio import get_singlefolder_fullfilename
 
-def pic_to_pdf(pdf_filename: str, pic_path: list) -> None:
+def pic_to_pdf(pdf_filename: str, pic_path_list: list) -> None:
 	"""
+	将多个图片合成一个pdf
+	（注意：这种方式按照第一张图片的尺寸生成pdf模板，比较适用于图片尺寸相等的情况）
+
 	:param pdf_filename : 生成的pdf绝对路径
     :type  pdf_filename : str
 
-    :param pic_path: 由图片的绝对路径组成的列表
-    :type  pic_path: list
+    :param pic_path_list: 由图片的绝对路径组成的列表
+    :type  pic_path_list: list
 
 	"""
 	# 打开第一个图片返回一个PIL对象
-	cover = Image.open(pic_path[0])
+	cover = Image.open(pic_path_list[0])
 	# 获取第一个图片的宽高参数
 	width, height = cover.size
 	# 按照上面的宽高参数生成一个空白的fpdf对象
-	pdf = FPDF(unit = "pt", format = [width, height])
+	pdf = FPDF(unit = "pt",format=(width,height))
 	# 遍历所有图片
-	for pic in pic_path:
+	for pic in pic_path_list:
 		# pdf对象添加新页面并放置图片
 		pdf.add_page()
 		pdf.image(pic, 0, 0)
 	#pdf另存为
 	pdf.output(pdf_filename, "F")
 
-pic_list = get_singlefolder_fullfilename(r"D:\MyNutstore\PersonalStudy\Python\WorkOverflow\tests\images",
-										 ['.JPG','.jpg','.jpeg','.gif','.png','.PNG'])
+def combine2Pdf(pdf_filename:str,pic_path:str) -> None :
+	"""
+	将多个图片合成一个pdf
+	（注意：这种方式按照pic_size将图片进行缩放，适合大小不一的图片）
+
+	:param pdf_filename : 生成的pdf绝对路径
+    :type  pdf_filename : str
+
+    :param pic_path: 图片的绝对路径
+    :type  pic_path: str
+	"""
+	# 设置图片默认尺寸，A4默认(96ppi)
+	pic_size = (794, 1123)
+	# 获取文件夹下所有图片路径
+	png_files = get_singlefolder_fullfilename(pic_path,
+											 ['.JPG','.jpg','.jpeg','.gif','.png','.PNG'])
+	sources = []
+	# 按名称排序
+	png_files.sort()
+	output = Image.open(png_files[0])
+	png_files.pop(0)
+	# 每张图片读取为PIL对象，并进行缩放至默认尺寸
+	for file in png_files:
+		png_file = Image.open( file )
+		w,h = png_file.size
+		# 计算缩放比例
+		n = w / pic_size[0] if (w / pic_size[0]) >= (h / pic_size[1]) else h / pic_size[1]
+		# 缩放图片
+		png_file.thumbnail((w / n, h / n))
+		# 需调整格式
+		if png_file.mode == "RGBA":
+			png_file = png_file.convert("RGB")
+			sources.append(png_file)
+		else:
+			sources.append(png_file)
+	# 另存为pdf
+	output.save(pdf_filename, "pdf", resolution=100.0, save_all=True, append_images=sources)
+
+if __name__ == "__main__":
+	folder = r"D:\MyNutstore\PersonalStudy\Python\WorkOverflow\tests\images"
+	pdffile = "result1111.pdf"
+	combine2Pdf(pdffile, folder)
 
 
-if __name__ == '__main__':
-	pic_to_pdf("result.pdf",pic_list)
+#
+# if __name__ == '__main__':
+# 	pic_list = get_singlefolder_fullfilename(r"D:\MyNutstore\PersonalStudy\Python\WorkOverflow\tests\images",
+# 											 ['.JPG','.jpg','.jpeg','.gif','.png','.PNG'])
+# 	pic_to_pdf("resultx.pdf",pic_list)
