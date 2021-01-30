323ca404109f593fa1d96acca8267f707fe49eef
karinlee <263384909@qq.com>
2021-01-30 19:20:09 +0800

    test:新增测试

---
+++ tests/oop_tests.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/1/3 13:26
+# @Author : 李加林
+# @FileName : oop_tests.py
+# @Software : PyCharm
+# @Blog : https://blog.csdn.net/weixin_43972976
+# @github : https://github.com/karinlee1988/
+# @gitee : https://gitee.com/karinlee/
+# @Personal website : https://karinlee.cn/
+
+class Base(object):
+
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        print(self.a + self.b)
+
+
+class New(Base):
+
+    def __init__(self,x,y,c):
+        super(New, self).__init__(x,y)
+        self.c = c
+
+    @property
+    def muti(self):
+        print(self.b * self.c)
+
+
+
+
+
+i = New(2,3,4)
+i.muti
+i.sum()
